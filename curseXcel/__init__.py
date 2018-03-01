6f701cb0c5ab17e1025b40b89feebc2844ea38c5
coffeeandscripts <erin.saricilar@gmail.com>
2018-03-02 09:45:19 +1100

    Package changes

--- curseXcel/__init__.py
+++ curseXcel/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["curseXcel"]
+from .curseXcel import Table
--- setup.py
+++ setup.py
@@ -5,20 +5,13 @@ import platform, warnings
 #Pypy dependency support
 python_implementation = platform.python_implementation()
 
-install_requires = ['ncurses',]
-if python_implementation == "PyPy":
-    install_requires = ['ncurses',]
-elif python_implementation != "CPython":
-    warnings.warn("We don't know how to deal with the {} runtime. Treating it like CPython".format(python_implementation), RuntimeWarning)
-
 setup(name='curseXcel',
     version='0.0.1',
     description='ncurses library to generate and manipulate tables',
     url='http://github.com/coffeeandscripts/curseXcel',
     author='coffeeandscripts',
+    packages=['curseXcel'],
     author_email='coffeeandscripts@gmail.com',
     license='GNU',
-    packages=['curseExcel',],
-    install_requires=install_requires,
     include_package_data=True
 )
